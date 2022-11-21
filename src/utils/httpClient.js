30e1b41287acb4bb17431bb5114f2d9c70d2e838
Waldir Merjildo <muwaldir@gmail.com>
2022-11-20 20:23:11 -0500

    modificaicon de las claves

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,5 +1,7 @@
-const API = process.env.REACT_APP_API;
-const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+// const API = process.env.REACT_APP_API;
+// const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+const API = 'https://api.themoviedb.org/3';
+const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs';
 
 function get(path) {
   return fetch(API + path, {
